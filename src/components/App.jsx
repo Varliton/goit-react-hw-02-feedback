96b629a4a11d3a0f87c884b78e5a15c736e8eb75
Varliton <nbmusic2405@gmail.com>
2023-01-22 23:18:20 +0200

    add styled components, formated with prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,44 @@
-import { FeedbackCounter } from "./FeedbackCounter/FeedbackCounter";
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
 
-export const App = () => {
-  return <div><FeedbackCounter/></div>;
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleFeedback = event => {
+    const feedbackOption = event.target.value;
+    this.setState(prevState => ({
+      [feedbackOption]: prevState[feedbackOption] + 1,
+    }));
+    this.countPositiveFeedbackPercentage();
+    this.countTotalFeedback();
+  };
+  countTotalFeedback = () => {
+    this.setState(prevState => ({
+      total: prevState.good + prevState.bad + prevState.neutral,
+    }));
+  };
+  countPositiveFeedbackPercentage = () => {
+    this.setState(prevState => ({
+      positivePercentage: Math.round((prevState.good * 100) / prevState.total),
+    }));
+  };
+  render() {
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions onLeaveFeedback={this.handleFeedback} />
+        <Statistics
+          good={this.state.good}
+          bad={this.state.bad}
+          neutral={this.state.neutral}
+          total={this.state.total}
+          positivePercentage={this.state.positivePercentage}
+        />
+      </Section>
+    );
+  }
+}
